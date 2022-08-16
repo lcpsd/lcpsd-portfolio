b39ae902afc12adb63da8bae666e5afc2a937f2e
Lucas Cardoso <lc@Lucass-MacBook-Pro.local>
2022-08-16 18:14:48 -0400

    comp: video in header bg

---
+++ src/components/videoBg.tsx
@@ -0,0 +1,28 @@
+import { ChakraStyledOptions, Flex } from "@chakra-ui/react";
+
+interface VideoBgProps extends ChakraStyledOptions {
+    source: string;
+}
+
+export function VideoBg({ source, ...rest }: VideoBgProps) {
+
+    return (
+        <Flex w='100%' h='100%' justify='center' align='center' {...rest} objectFit='cover' opacity="0.1">
+            <video
+                preload="auto"
+                autoPlay
+                muted
+                loop
+                playsInline
+                style={{
+                    height: 'auto',
+                    width: '100%',
+                    objectFit: 'cover',
+                    mixBlendMode: "hard-light"
+                }}
+                id='video'>
+                <source src={source} type='video/mp4' />
+            </video>
+        </Flex >
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,24 @@ import { FiMapPin, FiGithub, FiLinkedin } from "react-icons/fi"
 import { FloatingIcon } from '../components/FloatingIcon'
 import Link from 'next/link'
 import { Profile } from '../components/Profile'
+import { VideoBg } from '../components/videoBg'
 
 const Home: NextPage = () => {
   return (
-    <Flex direction="column" h="100%" w="100%" maxW="1280px" mx="auto" px={10} justify="center">
+    <Flex
+      direction="column"
+      h="100%"
+      w="100%"
+      maxW="1280px"
+      mx="auto"
+      px={10}
+      justify="center"
+      position="unset"
+      zIndex="1"
+    >
       <Header />
 
-      <Flex height="calc(100vh - 80px)">
+      <Flex height="calc(100vh - 80px)" zIndex="1">
         <HeaderTitle />
 
         <Flex flex="1" direction="column" align="center" justify="center" gap={10}>
@@ -23,6 +34,16 @@ const Home: NextPage = () => {
             location='Campo Grande - MS'
           />
         </Flex>
+      </Flex>
+
+      <Flex
+        position="absolute"
+        h="100%"
+        w='100vw'
+        left="0"
+        top="0"
+      >
+        <VideoBg source='https://i.imgur.com/HoH9j5S.mp4' opacity={{ base: '0.5', xl: '1' }} objectFit='cover' />
       </Flex>
     </Flex>
   )
