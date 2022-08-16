e47b4fa30914f6c78f589ddd4bb2f26052a7a6b3
Lucas Cardoso <lc@Lucass-MacBook-Pro.local>
2022-08-16 15:53:40 -0400

    basic config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app'
 
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
