f3a7a737cf12474db89c4f68c3da1dd406b4d7bb
Lucas Cardoso <lc@Lucass-MacBook-Pro.local>
2022-08-16 20:01:40 -0400

    comp: projects slide

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, Grid, Img, SimpleGrid, Text } from '@chakra-ui/react'
 import type { NextPage } from 'next'
+import { A11y, Pagination } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import { HeaderSection } from '../components/HeaderSection'
+import { ProjectsSlide } from '../components/ProjectsSlide'
 
 const Home: NextPage = () => {
+
+
   return (
     <Flex
       direction="column"
@@ -17,6 +22,8 @@ const Home: NextPage = () => {
 
       <Flex direction="column" h="100vh" align="center" py={10}>
         <Text color="primary" fontSize="3xl">Projetos</Text>
+
+        <ProjectsSlide />
       </Flex>
 
     </Flex >
