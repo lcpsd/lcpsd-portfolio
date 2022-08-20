54c02d867db31c2315be0912655847412a6a2c69
Lucas Cardoso <lucascardosopsd@gmail.com>
2022-08-20 19:54:37 -0400

    feat: fetch profile data from aws

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,14 +5,15 @@ import { FloatingIcon } from "./FloatingIcon";
 import { ProfilePicture } from "./ProfilePicture";
 import { DataStore } from '@aws-amplify/datastore';
 import { Profile as ProfileModel } from '../models';
-import { useEffect } from "react";
-import { ProfileProps } from "../types/profileProps";
+import { useEffect, useState } from "react";
 
-export function Profile({ pictureUrl, linkedinUrl, githubUrl, location, ...rest }: ProfileProps) {
+export function Profile() {
+
+    const [profileData, setProfileData] = useState<ProfileModel>()
 
     async function FetchUserProfile() {
-        const data = await DataStore.query(ProfileModel)
-        console.log(data)
+        const data = await DataStore.query<ProfileModel>(ProfileModel)
+        setProfileData(data[0])
     }
 
     useEffect(() => {
@@ -24,18 +25,18 @@ export function Profile({ pictureUrl, linkedinUrl, githubUrl, location, ...rest 
             <ProfilePicture pictureUrl='images/profile-picture.jpeg' />
 
             <Flex w="100%" justify="center" py={5} gap={5}>
-                <Link href={githubUrl}>
+                <Link href={profileData?.github ?? ""}>
                     <FloatingIcon icon={FiGithub} />
                 </Link>
 
-                <Link href={linkedinUrl}>
+                <Link href={profileData?.linkedIn ?? ""}>
                     <FloatingIcon icon={FiLinkedin} />
                 </Link>
             </Flex>
 
             <Flex align="center" justify="center">
                 <Icon as={FiMapPin} fontSize="2rem" />
-                <Text fontSize="2xl" color="primary">{location}</Text>
+                <Text fontSize="2xl" color="primary">{profileData?.location ?? ""}</Text>
             </Flex>
         </Box>
     )
--- src/types/profileProps.ts
+++
@@ -1,9 +0,0 @@
-import { ChakraStyledOptions } from "@chakra-ui/react";
-
-export interface ProfileProps extends ChakraStyledOptions {
-    avatarUrl: string;
-    linkedin: string;
-    github: string;
-    whatsapp: string;
-    location: string;
-}
