f77df814a993312f6f6b85ae64a187581e65925c
Lucas Cardoso <lucascardosopsd@gmail.com>
2022-08-24 14:37:42 -0400

    ignore eslint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   swcMinify: true,
   images: {
     domains: ["i.imgur.com"]
