0e458716d98b0a93409411df2c18c6740fd037cb
Lucas <lucas@MacBook-Pro-de-Lucas.local>
2022-10-20 10:44:39 -0400

    fix: page title

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -23,8 +23,8 @@ export default class MyDocument extends Document {
         ),
         render: (
           <Html>
-            <title>Lucas Cardoso Dev</title>
             <Head>
+              <title>Lucas Cardoso Dev</title>
               <link rel="preconnect" href="https://fonts.googleapis.com" />
               <link rel="preconnect" href="https://fonts.gstatic.com" />
               <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700" rel="stylesheet" />
